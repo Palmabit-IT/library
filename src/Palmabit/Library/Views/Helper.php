14c06bf12e5bd2150dc0fa37e60ba2a56aa3765d
Jacopo <beschi.jacopo@gmail.com>
2014-02-19 09:34:41 +0100

    view helper

---
+++ src/Palmabit/Library/Views/Helper.php
@@ -0,0 +1,31 @@
+<?php namespace Palmabit\Library\Views;
+/**
+ * Class View Helper
+ *
+ * @author jacopo beschi j.beschi@palmabit.com
+ */
+class Helper 
+{
+    /**
+     * Check if url is the same of the current url and return active state
+     * @param String $url
+     * @param String $active
+     * @return $active
+     */
+    public static function get_active($url, $active = 'active')
+    {
+        return (Request::url() == $url) ? $active : '';
+    }
+
+    /**
+     * Check if route name is the same as the current url and returns active state
+     * @param $match
+     * @param $active
+     */
+    public static function get_active_name($match, $active = 'active')
+    {
+        $route_name = Route::currentRouteName();
+        $base_name = array_values(explode(".", $route_name))[0];
+        return (strcasecmp($base_name, $match) == 0) ? $active : '';
+    }
+} 
