66b7e1a35ac9316d6d3e5b4d0db2233a68b69b19
neomusic <g.aremare@palmabit.com>
2015-11-09 17:05:19 +0100

    add function fit

--- src/Palmabit/Library/Image/ImageProcessor.php
+++ src/Palmabit/Library/Image/ImageProcessor.php
@@ -1,64 +1,66 @@
 <?php
 
+  namespace Palmabit\Library\Images;
 
-namespace Palmabit\Library\Images;
+  use Illuminate\Support\Facades\File;
+  use Illuminate\Support\MessageBag;
+  use Palmabit\Library\Exceptions\ImageEvidenceException;
 
+  class ImageProcessor {
+    protected $height = 50;
+    protected $width = 50;
+    protected $path = '/';
 
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\MessageBag;
-use Palmabit\Library\Exceptions\ImageEvidenceException;
+    protected $helper;
+    protected $image;
 
-class ImageProcessor
-{
-	protected $height = 50;
-	protected $width = 50;
-	protected $path = '/';
+    function __construct($image) {
+      $this->helper = new ImageHelper();
+      $this->image  = $image;
+    }
 
+    public function crop() {
+      return $this->helper->croptImage($this->image, $this->height, $this->width);
+    }
 
-	protected $helper;
-	protected $image;
+    public function resize() {
 
+      return $this->helper->resizeImage($this->image, $this->height, $this->width);
+    }
 
-	function __construct($image)
-	{
-		$this->helper = new ImageHelper();
-		$this->image = $image;
-	}
+    public function resizeByRatio() {
+      $ratio = $this->getRatio();
+      if (intval($this->image->width() / $ratio > $this->image->height())) {
+        return $this->image->fit(intval($this->image->height() * $ratio), $this->image->height());
+      } else {
+        return $this->image->fit($this->image->width(), intval($this->image->width() / $ratio));
+      }
+    }
 
-	public function crop()
-	{
-		return $this->helper->croptImage($this->image,$this->height,$this->width);
-	}
+    public function getRatio() {
+      return $this->height / $this->width;
+    }
 
-	public function resize()
-	{
+    public function cropAndResize() {
+      return $this->crop($this->resize());
+    }
 
-		return $this->helper->resizeImage($this->image,$this->height,$this->width);
-	}
+    public function save($filename) {
 
-	public function cropAndResize()
-	{
-		return $this->crop($this->resize());
-	}
+      if (!File::exists(public_path() . $this->path)) {
+        File::makeDirectory(public_path() . $this->path, $mode = 0777, true, true);
+      }
+      $relativePath = $this->path . $filename;
+      $this->image->save(public_path() . $relativePath);
 
-	public function save($filename)
-	{
+      return $relativePath;
+    }
 
-		if (!File::exists(public_path() . $this->path)) {
-			File::makeDirectory(public_path() . $this->path, $mode = 0777, true, true);
-		}
-		$relativePath = $this->path . $filename;
-			$this->image->save(public_path() . $relativePath);
+    public static function delete($filePath) {
+      if (!File::delete(public_path() . $filePath)) {
+        throw new ImageEvidenceException(new MessageBag(['Errore nell\'eliminazione dell\'immagine']));
+      }
+      return true;
+    }
 
-		return $relativePath;
-	}
-
-	public static function delete($filePath)
-	{
-		if(!File::delete(public_path() . $filePath)){
-			throw new ImageEvidenceException(new MessageBag(['Errore nell\'eliminazione dell\'immagine']));
-		}
-		return true;
-	}
-
-}
+  }
